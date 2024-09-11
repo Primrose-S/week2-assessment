da4d631f0c74e6dcc56297a148d6900e13a257a6
Primrose-S <primrosedube71@gmail.com>
2024-09-11 12:32:08 +0200

    added the coding

---
+++ week2.py
@@ -0,0 +1,19 @@
+my_list = []
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print(my_list)
+my_list.insert(1, 15)
+print(my_list)
+
+my_list.extend([50, 60, 70])
+print(my_list)
+
+my_list.pop(7)
+print(my_list)
+
+my_list.sort()
+print(my_list)
+
+print(my_list[3])
